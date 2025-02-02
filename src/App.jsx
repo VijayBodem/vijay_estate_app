dbc4f7316aabd56f16809c9e091a72ba790ff923
Vijay <vijaybodem17@gmail.com>
2025-02-02 13:00:29 +0530

    routing setup using react router dom new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./pages/home/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Listpage from "./pages/listpage/Listpage";
+import Layout from "./pages/layout/layout";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <Listpage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/listpage/Listpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Listpage = () => {
+  return <div>listpage</div>;
+};
+
+export default Listpage;
